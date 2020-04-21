852505ae9fa547afbeaf77e57e3a86734701fb59
amreshv007 <vamresh6@gmail.com>
2020-04-21 09:46:36 +0530

    models.py

--- Project1/apps/models.py
+++ Project1/apps/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
-
+# from django.contrib.auth.models import User
 
 class ImgPlace(models.Model):
     # person = models.ForeignKey(User, on_delete=models.CASCADE),
